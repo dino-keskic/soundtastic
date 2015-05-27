03ee99eb66d2fceb4cbcd34e934ee6ad4940bcba
Markus Peißl <markpeissl@gmx.at>
2015-05-27 12:20:04 +0200

    [MP,MS] Further MI Buttons, 2 tracks

--- Soundtasitc/app/src/main/java/soundtastic/soundtasitc/MixingInterface.java
+++ Soundtasitc/app/src/main/java/soundtastic/soundtasitc/MixingInterface.java
@@ -15,6 +15,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.SeekBar;
 import android.widget.Spinner;
@@ -31,8 +32,10 @@ public class MixingInterface extends Activity implements View.OnClickListener {
     public int track_nr;
 
     ImageButton buttonPlayAll;
-    ImageButton buttonTrackPlay;
+    ImageButton buttonTrackPlay1;
+    ImageButton buttonTrackPlay2;
     ImageButton buttonAddRec1;
+    ImageButton buttonAddRec2;
     ImageButton buttonAddSounds;
     Button buttonDeleteTrack;
 
@@ -46,16 +49,20 @@ public class MixingInterface extends Activity implements View.OnClickListener {
         track_nr = 0;
 
         buttonPlayAll = (ImageButton) findViewById(R.id.mi_play_all);
-        buttonTrackPlay = (ImageButton) findViewById(R.id.mi_track_play);
-        buttonAddRec1 = (ImageButton) findViewById(R.id.mi_track_rec);
+        buttonTrackPlay1 = (ImageButton) findViewById(R.id.mi_track_play1);
+        buttonTrackPlay2 = (ImageButton) findViewById(R.id.mi_track_play2);
+        buttonAddRec1 = (ImageButton) findViewById(R.id.mi_track_rec1);
+        buttonAddRec2 = (ImageButton) findViewById(R.id.mi_track_rec2);
         buttonAddSounds = (ImageButton) findViewById(R.id.mi_add_sounds);
 
         buttonTrackTitle1 = (TextView) findViewById(R.id.mi_track_title1);
         buttonDeleteTrack = (Button) findViewById(R.id.mi_track_delete);
 
         buttonPlayAll.setOnClickListener(this);
-        buttonTrackPlay.setOnClickListener(this);
+        buttonTrackPlay1.setOnClickListener(this);
+        buttonTrackPlay2.setOnClickListener(this);
         buttonAddRec1.setOnClickListener(this);
+        buttonAddRec2.setOnClickListener(this);
         buttonTrackTitle1.setOnClickListener(this);
         buttonDeleteTrack.setOnClickListener(this);
         buttonAddSounds.setOnClickListener(this);
@@ -94,7 +101,14 @@ public class MixingInterface extends Activity implements View.OnClickListener {
         Intent record = new Intent(this, RecordInterface.class);
 
         switch(v.getId()) {
-            case R.id.mi_track_rec:
+            case R.id.mi_track_rec1:
+                //record.putExtra("track_nr",track_nr);
+                // so abfangen int trackNr = record.getIntExtra("track_nr", 0);
+                startActivity(record);
+                break;
+            case R.id.mi_track_rec2:
+                //record.putExtra("track_nr",track_nr);
+                // so abfangen int trackNr = record.getIntExtra("track_nr", 0);
                 startActivity(record);
                 break;
             case R.id.mi_track_delete:
@@ -102,6 +116,9 @@ public class MixingInterface extends Activity implements View.OnClickListener {
                 break;
             case R.id.mi_track_title1:
                 track_nr = 1;
+                break;
+            case R.id.mi_track_title2:
+                track_nr = 2;
                 break;
             case R.id.mi_add_sounds:
                 addSounds();
@@ -116,19 +133,36 @@ public class MixingInterface extends Activity implements View.OnClickListener {
         // super.onBackPressed(); // Comment this super call to avoid calling finish()
     }
 
+    public void setTrackTitle(String track_title)
+    {
+       buttonTrackTitle1.setText(track_title);
+    }
+
     public void deleteTrack()
     {
-        buttonTrackPlay.setVisibility(View.INVISIBLE);
+
     }
 
     public void addSounds()
     {
         final Dialog dialog = new Dialog(this);
 
-        Button mic = (Button) dialog.findViewById(R.id.mi_add_sounds_mic);
-
         dialog.setContentView(R.layout.add_sounds);
         dialog.setTitle("Add new track");
         dialog.show();
+
+        final Button mic = (Button) dialog.findViewById(R.id.mi_add_sounds_mic);
+        final EditText title = (EditText) dialog.findViewById(R.id.mi_add_sounds_title);
+
+        if(mic != null)
+        {
+            mic.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    setTrackTitle(title.getText().toString());
+                    dialog.cancel();
+                }
+            });
+        }
     }
 }

022c603e7f0080c912139e39099901e02ab039ed
MichaelPlanitzer <michael.planitzer@student.tugraz.at>
2015-06-10 14:24:33 +0200

    [MP] midi2gridView_gridView2midi

--- Soundtasitc/app/src/main/java/soundtastic/soundtasitc/GridView.java
+++ Soundtasitc/app/src/main/java/soundtastic/soundtasitc/GridView.java
@@ -16,6 +16,7 @@ import android.view.View;
 import android.widget.FrameLayout;
 import android.widget.Button;
 import android.widget.HorizontalScrollView;
+import android.widget.ImageButton;
 import android.widget.ScrollView;
 import android.widget.TableLayout;
 import android.widget.TableRow;
@@ -38,15 +39,23 @@ public class GridView extends Activity{
             "c''''''"};
 
     int numTones = notes.length;
-    int midiLength = 20;
+    //List<Integer> rawMidiValues = ProjectInfos.getInstance().getProject().getTrackByName(ProjectInfos.getInstance().getSelectedTrack().getTrackName()).getRawMidiValuesList();
+    List<Integer> rawMidiValues = new ArrayList<Integer>();
+    List<Integer> outputMidiValues = new ArrayList<Integer>();
+    int midiLength;
+    int numBar;
+    int gridLength;
+
     FrameLayout gridViewLayout;
-    TextView[][] grid = new TextView[numTones][midiLength];
+    TextView[][] grid;
 
     Button buttonIncrHeight;
     Button buttonDecrHeight;
     Button buttonIncrLength;
     Button buttonDecrLength;
     Button buttonNew_Delete;
+    ImageButton buttonSave;
+    ImageButton buttonCancel;
 
     TextView clickedTextView;
     ArrayList<TextView> clickedTextViewListAdd = new ArrayList<TextView>();
@@ -62,15 +71,34 @@ public class GridView extends Activity{
         buttonIncrLength = (Button) findViewById(R.id.IncrLength);
         buttonDecrLength = (Button) findViewById(R.id.DecrLength);
         buttonNew_Delete = (Button) findViewById(R.id.New_Delete);
+        buttonSave = (ImageButton) findViewById(R.id.gridview_save);
+        buttonCancel = (ImageButton) findViewById(R.id.gridview_cancel);
 
         buttonIncrHeight.setOnClickListener(buttonHandler);
         buttonDecrHeight.setOnClickListener(buttonHandler);
         buttonIncrLength.setOnClickListener(buttonHandler);
         buttonDecrLength.setOnClickListener(buttonHandler);
         buttonNew_Delete.setOnClickListener(buttonHandler);
+        buttonSave.setOnClickListener(buttonHandler);
+        buttonCancel.setOnClickListener(buttonHandler);
 
+        buttonIncrLength.setEnabled(false);
+        buttonDecrLength.setEnabled(false);
+
+        for (int i = getResources().getInteger(R.integer.midi_nr_offset_min); i < 100; i++)
+            rawMidiValues.add(i);
+
+        midiLength = rawMidiValues.size();
+        numBar = 1 + midiLength / getResources().getInteger(R.integer.bar);
+        gridLength = midiLength + numBar;
+        grid = new TextView[numTones][gridLength];
         createGrid();
+        writeMidi2Grid();
     }
+
+    @Override
+    public void onBackPressed()
+    {}
 
     public void createGrid()
     {
@@ -84,11 +112,11 @@ public class GridView extends Activity{
         {
             TableRow tbrow = new TableRow(this);
 
-            for(int j=0; j<midiLength; j++)
+            for(int j=0; j<gridLength; j++)
             {
                 TextView tv1 = new TextView(this);
                 tv1.setId(i * midiLength + j);
-                if (j%16 == 0)
+                if (j%getResources().getInteger(R.integer.bar_plus_1) == 0)
                 {
                     grid[i][j] = tv1;
                     tv1.setText(notes[i]);
@@ -98,9 +126,7 @@ public class GridView extends Activity{
                     grid[i][j] = tv1;
                     grid[i][j].setOnClickListener(textViewHandler);
                     tv1.setBackgroundColor(Color.WHITE);
-                    tv1.setText("WH");
-                    tv1.setTextColor(Color.WHITE);
-                    tv1.setBackground(getResources().getDrawable(R.drawable.border));
+                    drawWhite(tv1);
                 }
                 tbrow.addView(tv1);
             }
@@ -111,32 +137,48 @@ public class GridView extends Activity{
         gridViewLayout.addView(sv);
     }
 
+    public void writeMidi2Grid()
+    {
+        int time_position = 0;
+        for (Integer tone : rawMidiValues)
+        {
+            if ((tone >= getResources().getInteger(R.integer.midi_nr_offset_min)) && (tone <= getResources().getInteger(R.integer.midi_nr_offset_max)))
+            {
+                int num_bar = time_position / getResources().getInteger(R.integer.bar);
+                TextView position = grid[(tone - getResources().getInteger(R.integer.midi_nr_offset_min))][1 + time_position + num_bar];
+                drawGreen(position);
+                time_position++;
+            }
+        }
+
+    }
+
     public void drawWhite(TextView tv)
     {
-        tv.setText("WH");
-        tv.setTextColor(Color.WHITE);
+        tv.setText(R.string.white);
+        tv.setTextColor(getResources().getColor(R.color.white));
         tv.setBackground(getResources().getDrawable(R.drawable.border));
     }
 
     public void drawGreen(TextView tv)
     {
-        tv.setText("GR");
-        tv.setTextColor(0x008000);
+        tv.setText(R.string.green);
+        tv.setTextColor(getResources().getColor(R.color.green));
         tv.setBackground(getResources().getDrawable(R.drawable.border_green));
     }
 
     public void drawBlue(TextView tv)
     {
-        tv.setText("BL");
-        tv.setTextColor(0x0000FF);
+        tv.setText(R.string.blue);
+        tv.setTextColor(getResources().getColor(R.color.blue));
         tv.setBackground(getResources().getDrawable(R.drawable.border_new));
 
     }
 
     public void drawYellow(TextView tv)
     {
-        tv.setText("YE");
-        tv.setTextColor(0xFFFF00);
+        tv.setText(R.string.yellow);
+        tv.setTextColor(getResources().getColor(R.color.yellow));
         tv.setBackground(getResources().getDrawable(R.drawable.border_edit));
     }
 
@@ -163,19 +205,19 @@ public class GridView extends Activity{
 
             clickedTextView = (TextView) v;
 
-            if (clickedTextView.getText() == "WH")
+            if (clickedTextView.getText() == getResources().getString(R.string.white))
             {
                 disableButtons();
                 buttonNew_Delete.setEnabled(true);
                 clickedTextViewListAdd.add(clickedTextView);
                 drawBlue(clickedTextView);
             }
-            else if(clickedTextView.getText() == "BL")
+            else if(clickedTextView.getText() == getResources().getString(R.string.blue))
             {
                 clickedTextViewListAdd.remove(clickedTextView);
                 drawWhite(clickedTextView);
             }
-            else if(clickedTextView.getText() == "YE")
+            else if(clickedTextView.getText() == getResources().getString(R.string.yellow))
             {
                 clickedTextViewList.remove(clickedTextView);
                 drawGreen(clickedTextView);
@@ -183,6 +225,8 @@ public class GridView extends Activity{
             else
             {
                 enableButtons();
+                buttonIncrLength.setEnabled(false);
+                buttonDecrLength.setEnabled(false);
                 drawYellow(clickedTextView);
                 clickedTextViewList.add(clickedTextView);
             }
@@ -190,8 +234,8 @@ public class GridView extends Activity{
     };
     View.OnClickListener buttonHandler = new View.OnClickListener() {
         public void onClick(View v) {
-            Button clickedButton = (Button) v;
-            switch(clickedButton.getId())
+            //Button clickedButton = (Button) v;
+            switch(v.getId())
             {
                 case R.id.IncrHeight:
                     increaseHeight();
@@ -208,24 +252,49 @@ public class GridView extends Activity{
                 case R.id.New_Delete:
                     newDeleteTone();
                     break;
+                case R.id.gridview_save:
+                    writeGrid2Midi();
+                    finish();
+                    break;
+                case R.id.gridview_cancel:
+                    finish();
+                    break;
+
             }
         }
     };
+
+    public void writeGrid2Midi()
+    {
+        for (int time_position = 0; time_position < gridLength; time_position++)
+        {
+            for (int tone = 0; tone < numTones; tone++)
+            {
+                TextView tv = grid[tone][time_position];
+                if (tv.getText() == getResources().getString(R.string.green))
+                {
+                    outputMidiValues.add(tone + getResources().getInteger(R.integer.midi_nr_offset_min));
+                    break;
+                }
+
+            }
+        }
+    }
 
     public void increaseHeight()
     {
         ArrayList<TextView> editedTextViewList = new ArrayList<TextView>();
         for (TextView textView : clickedTextViewList) {
             int clickedID = textView.getId();
-            int targedID = clickedID + midiLength;
-            if (targedID > numTones * midiLength)
-                targedID = clickedID;
+            int targetID = clickedID + midiLength;
+            if (targetID > numTones * midiLength)
+                targetID = clickedID;
 
-            TextView targedTextView = (TextView) findViewById(targedID);
+            TextView targetTextView = (TextView) findViewById(targetID);
             drawWhite(textView);
-            editedTextViewList.add(targedTextView);
-            if (targedTextView.getText() != "GR")
-                drawYellow(targedTextView);
+            editedTextViewList.add(targetTextView);
+            if (targetTextView.getText() != getResources().getString(R.string.green))
+                drawYellow(targetTextView);
         }
 
         clickedTextViewList.clear();
@@ -237,15 +306,15 @@ public class GridView extends Activity{
         ArrayList<TextView> editedTextViewList = new ArrayList<TextView>();
         for (TextView textView : clickedTextViewList) {
             int clickedID = textView.getId();
-            int targedID = clickedID - midiLength;
-            if (targedID < 0)
-                targedID = clickedID;
+            int targetID = clickedID - midiLength;
+            if (targetID < 0)
+                targetID = clickedID;
 
-            TextView targedTextView = (TextView) findViewById(targedID);
+            TextView targetTextView = (TextView) findViewById(targetID);
             drawWhite(textView);
-            editedTextViewList.add(targedTextView);
-            if (targedTextView.getText() != "GR")
-                drawYellow(targedTextView);
+            editedTextViewList.add(targetTextView);
+            if (targetTextView.getText() != getResources().getString(R.string.green))
+                drawYellow(targetTextView);
         }
 
         clickedTextViewList.clear();
